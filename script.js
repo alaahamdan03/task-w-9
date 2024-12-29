7e7c999675637efbc6ef0250b29081f11ddc04ca
alaahamdan03 <alaahamdan03@gmail.com>
2024-12-29 21:29:26 +0100

    add files

---
+++ script.js
@@ -0,0 +1,51 @@
+ 
+ // Answer 1
+
+var num1 = +prompt("first number");
+var num2 = +prompt("second number");
+sum = num1 + num2;
+document.write("<h2>the sum of plus : " + sum + " </h2>");
+
+
+
+
+// Answer 2
+var nam=+ prompt("Enter The   Number")
+var modu = nam%2
+if (modu ==0) {
+    document.write(" the number is even"  );
+  } else {
+    document.write(" the number is odd"  );
+  }
+
+
+
+// Answer 3 
+var num1=+prompt("enter first number")
+var num2=+prompt("enter sacond number")
+if(number1>number2)
+{
+document.write("number1 is greater");
+}
+else(number1<number2)
+{
+document.write("<h2>number2 is lesser");
+}
+
+
+// Answer 4
+var letter=prompt("Enter a  letter")
+switch (letter) {
+    case 'a':
+    case 'o':
+    case 'e':
+    case 'i':
+    case 'u':
+     document.write("vowel" );
+
+ break;
+    default:
+      document.write("constant");
+
+}
+      */
